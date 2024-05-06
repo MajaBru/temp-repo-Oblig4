0e3b6ce40442541ad55ea6babe8a1230ba0a8fd0
MajaBru <122542734+MajaBru@users.noreply.github.com>
2024-05-06 17:24:18 +0200

    new responsive navbar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, NavLink } from "react-router-dom";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as MdIcons from "react-icons/md";
+import * as PiIcons from "react-icons/pi";
+import { IconContext } from 'react-icons/lib';
 import './Header.css';
 
 const Nav = () => {
+
+    const [sidebar, setSidebar] = useState(false);
+
+    // toggle sidebar 
+    const showSidebar = () => setSidebar(!sidebar);
+
+    // accessibility -- allow to open menu with enter or spacebar
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter' || e.code === 'Space')  {
+            showSidebar();
+        }
+    };
+
     return (
+        <IconContext.Provider value={{ color: '#fff' }}>
         <header className='Main-Header'>
-            <nav>
+            <div className='navbar' on>
+                <Link to="#" className="menu-bars" onKeyDown={handleKeyPress}><FaIcons.FaBars onClick={showSidebar} /></Link>
+            </div>
+             <nav role="navigation" className={sidebar ? "nav-menu active" : "nav-menu"}>
                 <ul>
-                    <li><Link to="/dashboard" className="nav-item">Dashboard</Link></li>
-                    <li><Link to="/manage/cards" className="nav-item">Cards Management</Link></li>
-                    <li><Link to="/manage/users" className="nav-item">Users Management</Link></li>
-                    <li><Link to="/logout" className="nav-item">Log out</Link></li>
+                    <li aria-label='Close Menu' className='navbar-toggle'>
+                        <Link to='#' className='menu-bars' onClick={showSidebar}>
+                        <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>
+                    <li className="nav-item" aria-label='Dashboard' ><NavLink to="/dashboard"> <span><MdIcons.MdDashboard/></span> Dashboard</NavLink></li>
+                    <li className="nav-item" aria-label='Cards management'><NavLink to="/manage/cards"><span><PiIcons.PiCardsFill /></span> Cards Management</NavLink></li>
+                    <li className="nav-item" aria-label='Users management'><NavLink to="/manage/users"><span><FaIcons.FaUsers/></span>Users Management</NavLink></li>
+                    <li className="nav-item" aria-label='Log out'><NavLink to="/logout"><span><MdIcons.MdOutlineLogout /></span>Log out</NavLink></li>
                 </ul>
             </nav>
         </header>
+        </IconContext.Provider>
     );
     };
 
